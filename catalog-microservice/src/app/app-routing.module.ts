2462e43ef96af913be1e429b353116a8e6165e36
otoukebri <oussema.toukebri@sofrecom.com>
2018-11-22 13:46:16 +0100

    init work routing

--- catalog-microservice/src/app/app-routing.module.ts
+++ catalog-microservice/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'catalog', component: CatalogComponent },
+  {path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
