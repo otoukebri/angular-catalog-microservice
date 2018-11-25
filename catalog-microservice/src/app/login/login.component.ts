7c6a7dccbdb9b984eab75f58f6f2c9b6c0b536b8
otoukebri <oussema.toukebri@gmail.com>
2018-11-25 17:26:44 +0100

    feat(login) layout : center content

--- catalog-microservice/src/app/login/login.component.ts
+++ catalog-microservice/src/app/login/login.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  
   loginForm = new FormGroup({
-    mail: new FormControl(''),
-    password: new FormControl(''),
+    mail: new FormControl(""),
+    password: new FormControl("")
   });
-  
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor() {}
+
+  ngOnInit() {}
 
   onSubmit() {
     //console.warn(this.profileForm.value);
-    alert("bonjour!")
+    alert("bonjour!");
   }
-
 }
